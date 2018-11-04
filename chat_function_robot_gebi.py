0e2fc12c87893c58dc1c3e2c3252776272205792
huanglmw <[huanglmw@cn.ibm.com]>
2018-11-04 10:23:54 +0800

    i

--- chat_function_robot_gebi.py
+++ chat_function_robot_gebi.py
@@ -33,6 +33,9 @@ def main(login_nm, login_pwd, login_user, teacher, skill_name, is_debug=None, ):
                     logging.error('error in get hiz')
                     raise
 
+                if robot.bpz_flag:
+                    robot.update_bpz_status()
+
                 if dialogs:
                     try:
                         robot.response_to_roll(dialogs, show_all_msg=True)
